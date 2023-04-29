8e188e2a1fe29cedc16cfd43eccd908a5cbe3679
davidtazy <david.allemant@gmail.com>
2023-04-29 18:55:23 +0000

    Extract UpdateQuality in new Inn class and add non regression unit test

--- GildedRose.Cli/Program.cs
+++ GildedRose.Cli/Program.cs
@@ -5,131 +5,28 @@ namespace GildedRose.Cli
 {
     class Program
     {
-        IList<Item> Items;
+
         static void Main(string[] args)
         {
+
+            var inn = new Inn();
+
             System.Console.WriteLine("OMGHAI!");
-
-            var app = new Program()
-            {
-                Items = new List<Item>
-                                          {
-                                              new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20 },
-                                              new Item { Name = "Aged Brie", SellIn = 2, Quality = 0 },
-                                              new Item { Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7 },
-                                              new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 },
-                                              new Item
-                                                  {
-                                                      Name = "Backstage passes to a TAFKAL80ETC concert",
-                                                      SellIn = 15,
-                                                      Quality = 20
-                                                  },
-                                              new Item { Name = "Conjured Mana Cake", SellIn = 3, Quality = 6 }
-                                          }
-
-            };
-
-
             System.Console.WriteLine("Type <enter> to pass to the next day or <esc> to quit:");
             System.Console.WriteLine();
 
             while (System.Console.ReadKey().Key != ConsoleKey.Escape)
             {
                 System.Console.WriteLine("Go to next day...");
-                app.UpdateQuality();
+                inn.UpdateQuality();
             }
 
             System.Console.WriteLine("Ciao...");
             System.Console.ReadLine();
         }
 
-        public void UpdateQuality()
-        {
-            for (var i = 0; i < Items.Count; i++)
-            {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (Items[i].Quality > 0)
-                    {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            Items[i].Quality = Items[i].Quality - 1;
-                        }
-                    }
-                }
-                else
-                {
-                    if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].SellIn < 11)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-
-                            if (Items[i].SellIn < 6)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    Items[i].SellIn = Items[i].SellIn - 1;
-                }
-
-                if (Items[i].SellIn < 0)
-                {
-                    if (Items[i].Name != "Aged Brie")
-                    {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].Quality > 0)
-                            {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    Items[i].Quality = Items[i].Quality - 1;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                        }
-                    }
-                    else
-                    {
-                        if (Items[i].Quality < 50)
-                        {
-                            Items[i].Quality = Items[i].Quality + 1;
-                        }
-                    }
-                }
-            }
-        }
 
     }
 
-    /// <summary>
-    /// Forbidden: do not modify this class!!!
-    /// </summary>
-    public class Item
-    {
-        public string Name { get; set; }
 
-        public int SellIn { get; set; }
-
-        public int Quality { get; set; }
-    }
 }
--- GildedRose.Tests/TestAssemblyTests.cs
+++ GildedRose.Tests/TestAssemblyTests.cs
@@ -1,14 +1,29 @@
 ﻿namespace GildedRose.Tests
 {
+    using GildedRose.Cli;
     using NUnit.Framework;
 
     [TestFixture]
     public class TestAssemblyTests
     {
         [Test]
-        public void TestTheTruth()
+        public void ensure_no_regression_inserted_during_refactor()
         {
-            Assert.IsTrue(true);
+            var inn = new Inn();
+            var inn_leg = new InnLegacy();
+
+            System.Console.WriteLine(inn);
+            System.Console.WriteLine(inn_leg);
+
+            Assert.That(inn_leg.ToString(), Is.EqualTo(inn.ToString()).NoClip);
+
+            for (int i = 0; i < 16; i++)
+            {
+                inn.UpdateQuality();
+                inn_leg.UpdateQuality();
+
+                Assert.That(inn_leg.ToString(), Is.EqualTo(inn.ToString()).NoClip);
+            }
         }
     }
 }
